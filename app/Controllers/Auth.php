fccc6a8db4947cd4265c4919264bc112e531831c
r204 <60972509+r204@users.noreply.github.com>
2024-09-30 09:20:17 +0700

    Update Controller

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -6,14 +6,20 @@ class Auth extends BaseController
 {
     public function index()
     {
+        $data = [
+            "title" => "Sign In"
+        ];
         //return view('home/template/header');
-        return view('home/signin');
+        return view('home/signin', $data);
         //return view('home/template/footer');
     }
     public function signup()
     {
+        $data = [
+            "title" => "Sign Up"
+        ];
         //return view('home/template/header');
-        return view('home/signup');
+        return view('home/signup', $data);
         //return view('home/template/footer');
     }
 
--- app/Views/detailartikel.php
+++ app/Views/detailartikel.php
@@ -6,19 +6,27 @@
         <div class="justify-content-center ms-2">
 
             <h1><?= $artikel->judul; ?></h1>
-            <div class="carousel-item">
-                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <rect width="100%" height="100%" fill="#777" />
-                </svg>
-
-                <div class="container">
-                    <div class="carousel-caption">
-                        <h1>Another example headline.</h1>
-                        <p>Some representative placeholder content for the second slide of the carousel.</p>
-                        <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            <p class="text-muted">Dibuat Pada Tanggal : <?= date('d/M/Y', strtotime($artikel->created_at)) ?></p>
+            <p class="text-muted">Ditulis Oleh : <strong><?= $artikel->author; ?></strong></p>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="/img/<?= $artikel->img1; ?>" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/img/<?= $artikel->img2; ?>" class="d-block w-100" alt="...">
                     </div>
                 </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
+            <p><?= $artikel->body; ?></p>
         </div>
     </div>
 </div>
